e0ee88f9c21e033f68420657b2a5cfd6d9ccc719
ke152 <ke152@foxmail.com>
2022-07-07 22:35:03 +0800

    Add Connector Interface

---
+++ KKNet/KKNet/Connector/Interface.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KKNet.Connector;
+
+public interface IConnector<T>
+{
+    event Action<T> OnConnect;
+}
+
+public interface IConnector : IConnector<Socket>
+{
+
+}
+
+class TcpListener
---
+++ KKNet/KKNet/Connector/TcpConnector.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace KKNet.Connector;
+
+public class TcpConnector : IConnector
+{
+    public event Action<Socket>? OnConnect;
+}
