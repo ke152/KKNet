9d00aad796c13b04b798f3a86637c3d5734a0f53
ke152 <ke152@foxmail.com>
2022-07-09 09:41:36 +0800

    Create ISession.cs

---
+++ KKNet/KKNet/Session/ISession.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KKNet.Session;
+
+public interface ISession<T>
+{
+    public event Action<T> OnRecv;
+
+    public void Send(T message);
+}
+
